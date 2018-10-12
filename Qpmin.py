380ea3ac8466d38969242a260c749281e015f313
dmitrynvm <dmitrynvm@gmail.com>
2018-10-12 16:28:46 +0300

    auto

--- Qpmin.py
+++ Qpmin.py
@@ -1,48 +1,8 @@
-"""
-Tropical (min,+) p-adic semiring
-
-AUTHORS:
-
-- Dmitry Kashirin (2016-10-15) - Initial version
-- Dmitry Nikolayev (2016-10-15) - Improved version
-"""
-
 import math
-
-def int2list(x, p):
-	"""Converts nonnegative integer or infinity to a list 
-	in a given numeric base"""
-	digits = []
-	if x == 0: return [0]
-	elif x == math.inf:
-		return [math.inf]
-	while x:
-		digits.append(x % p)
-		x = x//p
-		digits.reverse()
-	return digits
-
-def list2int(x, p):
-	"""
-		Дописать
-	"""
-	pass
 
 
 class Qpmin:
 	def __init__(self, value = 0, p = 10):
-		"""
-        Initialization.
-
-        INPUT:
-
-            - value -- Base ring.
-            - p -- base of the p-adic semifield (natural number).
-
-        EXAMPLES::
-
-            sage: R = Zp(17,3) #creates new object with value=17 over Q_3.
-        """
 		self.value = value
 		self.p = p
 		self.coefs = int2list(value, p)
--- Zpmin.py
+++ Zpmin.py
@@ -1,22 +1,62 @@
 import math
 
+
+def convert(x, p):
+	digits = []
+	if x == 0: return [0]
+	elif x == math.inf:
+		return [math.inf]
+	while x:
+		digits.append(x % p)
+		x = x // p
+		digits.reverse()
+	return digits
+
+
 def nu(x, p):
     return int(str(math.ceil(math.log(x,p))),p)
 
+
 class Zpmin(object):
-    p = 4
-    def __init__(self, value):
+    p = 10
+    def __init__(self, value=math.inf):
         self.value = value
     def __add__(self, other):
-        return Zpmin(self.value * self.p**nu(other.value, self.p) + other.value)
-    def __mul__(self, other):
         return Zpmin(min(self.value, other.value))
+    def __mul__(self, other):
+        return Zpmin(self.value * self.p**nu(other.value, self.p) + other.value)
     def __str__(self):
         out = ''
-        out += str(int(str(self.value), self.p))
+        #out += str(convert(self.value, self.p))
+        out += str(self.value)
+        return out
+
+
+class Matrix(object):
+    def __init__(self, rows=0, cols=0, data=[], algebra=Zpmin):
+        self.rows = rows
+        self.cols = cols
+        if(data):
+            self.data = data
+        else:
+            for i in range(rows):
+                self.data.append([Zpmin()]*cols)
+    def __add__(self, other):
+        res = Matrix(self.rows, self.cols)
+        for i in range(res.rows):
+            for j in range(res.cols):
+                res.data[i][j] = other.data[i][j]
+    def __str__(self):
+        out = ''
+        for i in range(res.rows):
+            for j in range(res.cols):
+                out += res.data[i][j] + ' '
+            out << '\n'
         return out
 
 a = Zpmin(12)
 b = Zpmin(3)
-c = a + b
+c = a * b
 print('a =', c)
+print('a =', c + a)
+A = Matrix(2,2)
